ac5b8f18bf2475a661aea750b2d7e5859465b311
Mohammad Jabir <mohammadj94@gmail.com>
2023-05-07 17:03:04 -0600

    review complete.

--- DecimalVariables/Program.cs
+++ DecimalVariables/Program.cs
@@ -1,5 +1,7 @@
 ﻿
 // decimal is more precise than double (costs more)
+// primarily holds things like money and astronomical numbers
+// math operations - add/subtract
 
 // double = 4.12, 32, 1.234
 // decimal = 4.12, 32, 1.234
@@ -7,3 +9,6 @@
 // Int32 - 00000000 00000000 00000000 00000000
 
 decimal bankAccountBalance;
+
+bankAccountBalance = 2.34M;
+Console.WriteLine(bankAccountBalance);
