495d26b751805093aba4412621b742d4935df6a8
Mohammad Jabir <mohammadj94@gmail.com>
2023-05-07 16:59:04 -0600

    Add project files.

---
+++ DecimalVariables/Program.cs
@@ -0,0 +1,9 @@
+﻿
+// decimal is more precise than double (costs more)
+
+// double = 4.12, 32, 1.234
+// decimal = 4.12, 32, 1.234
+
+// Int32 - 00000000 00000000 00000000 00000000
+
+decimal bankAccountBalance;
